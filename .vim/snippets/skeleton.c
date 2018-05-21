bd6dbc7d501bbfb8182fd30421493fe5ed20453c
Leonardo Essia <leonardo@essia.dev>
2018-05-20 23:25:41 -0300

    Add all

---
+++ .vim/snippets/skeleton.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+* =================================================
+*    Filename:  .c
+* Description:
+*     Created:  dd/mm/18 hh:mm
+* =================================================
+*/
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
