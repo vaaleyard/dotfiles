13ff297df03554d484abdb3bddab768341030f92
Leonardo Essia <leonardo@essia.dev>
2018-05-20 23:27:16 -0300

    Add all

---
+++ .config/polybar/cmus.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+cmusstatus=$(cmus-remote -C status)
+grep position <<< "$cmusstatus" 1>/dev/null 2>&1
+if [ ! $? -eq 0 ]; then exit; fi
+
+prepend_zero () {
+    seq -f "%02g" $1 $1
+}
+
+strindex() { 
+  x="${1%%$2*}"
+  [[ "$x" = "$1" ]] && echo -1 || echo "${#x}"
+}
+
+get_all_but_first() {
+  shift
+  echo "$@"
+}
+
+get_stat() {
+  line=$(grep "$1" -m 1 <<< "$cmusstatus")
+  a=$(strindex "$line" "$1")
+  sub="${line:a}"
+  echo "$(get_all_but_first $sub)"
+}
+
+
+
+position=$(get_stat position)
+minutes1=$(prepend_zero $(($position / 60 )))
+seconds1=$(prepend_zero $(($position % 60)))
+
+duration=$(get_stat duration)
+minutes2=$(prepend_zero $(($position / 60)))
+seconds2=$(prepend_zero $(($duration % 60)))
+
+echo -n " $(cmus-remote -C status | grep file | cut -d'/' -f6 | sed 's/.mp3//') [$minutes1:$seconds1 / $minutes2:$seconds2]"
+
