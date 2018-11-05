c284782102fc2340dae6695c75c12de52ebfb44b
Leonardo F <leonardof@tutanota.com>
2018-11-05 20:38:15 -0200

    Update mappings for C on vim

--- .bin/khan-academy-courses.sh
+++ .bin/khan-academy-courses.sh
@@ -12,9 +12,7 @@ mkdir -p $CACHEDIR
 # 2.1 - Coloca a quantidade de views na frente do nome de cada camgirl (juntando a linha debaixo com)
 # 2.2 - adiciona a palavra viewers no final da linha
 
-curl -sL https://www.chaturbate.com/female-cams | sed -rne 's,^<a href="/(\w*)/">$,\1,p ; s/<li\ class="cams">[0-9]+ mins, ([0-9]+) viewers<\/li>/\1/p' | sed -rn 'N;s/\n/ - /; s/$/ viewers/p' > $CACHEFILE
-
-NUM=$(wc -l $CACHEFILE | cut -d' ' -f1)
+curl -sL https://www.chaturbate.com/female-cams | sed -rne 's,^<a href="/(\w*)/">$,\1,p ; s/^.*\s([0-9]+)\sviewers.*$/\1/p' | sed -rne 'N;s/\n/ - /; s/$/ viewers/p' > $CACHEFILE
 
 printf "Chaturbate - female cams\n"
 nl -w 1 $CACHEFILE
