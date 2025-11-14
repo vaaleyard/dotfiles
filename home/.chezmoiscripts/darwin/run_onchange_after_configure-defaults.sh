d08481e52b5b7849c6f5fbbb701bde3b797d5cc6
Leonardo Essia <leonardo@essia.dev>
2025-11-13 22:48:28 -0300

    add new install scripts for macOS

---
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure-defaults.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This script enables a set of "sane" defaults
+set -eufo pipefail
+
+# Disable all text "smart features"
+defaults write -g NSAutomaticCapitalizationEnabled -int 0
+defaults write -g NSAutomaticDashSubstitutionEnabled -int 0
+defaults write -g NSAutomaticInlinePredictionEnabled -int 0
+defaults write -g NSAutomaticPeriodSubstitutionEnabled -int 0
+defaults write -g NSAutomaticQuoteSubstitutionEnabled -int 0
+defaults write -g NSAutomaticSpellingCorrectionEnabled -int 0
+defaults write -g NSAutomaticTextCorrectionEnabled -int 0
+defaults write -g NSAutomaticWindowAnimationsEnabled -int 0
+
+defaults write -g NSDocumentSaveNewDocumentsToCloud -int 0
+defaults write -g NSUserDictionaryReplacementItems '()'
+defaults write -g WebAutomaticSpellingCorrectionEnabled -int 0
+
+defaults write -g InitialKeyRepeat -int 15
+defaults write -g KeyRepeat -int 2
+defaults write -g com.apple.keyboard.fnState -int 1
+
+defaults write -g com.apple.swipescrolldirection -int 0 # disable "natural" scrolling
+defaults write -g com.apple.trackpad.forceClick -int 0
+
+defaults write com.apple.dock autohide -int 1
+defaults write com.apple.dock show-recents -int 0
+
+defaults write com.apple.finder FXPreferredViewStyle -string Nlsv
+defaults write com.apple.finder _FXSortFoldersFirst -int 1
+defaults write com.apple.finder FXRemoveOldTrashItems -int 1
+defaults write com.apple.finder FXEnableExtensionChangeWarning -int 0
